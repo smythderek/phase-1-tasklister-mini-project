b456b78a6de1b7e4a8fe5b5b79cb6b5eab8f0255
Derek Smyth <smyth.derek@gmail.com>
2023-03-08 12:52:31 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let taskText = document.getElementById("new-task-description").value;
+    buildToDo(taskText);
+    form.reset();
+  })
+})
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X '
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
